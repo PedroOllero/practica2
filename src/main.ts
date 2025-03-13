cc2bfab3a73b5d0ab471ed9c7bfd0a34b53d959c
Pedro Ollero <pedroollero1998@gmail.com>
2025-03-13 12:05:40 +0100

    Update payment calculation to account for beverage costs instead of invitation costs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 let amigos: number = 6;
 let precioCena: number = 120;
-let invitoYo: number = 18;
+let bebidas: number = 18;
 
-const cuantoPago: number = (precioCena - invitoYo)/(amigos + 1);
+const cuantoPago: number = (precioCena - bebidas)/(amigos + 1);
 
 //Aquí paso a dos decimales
 let resultado: string = cuantoPago.toFixed(2)
