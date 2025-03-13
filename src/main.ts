07191f68216cac7fd751aa6cb62e8cfc9bfd8666
Pedro Ollero <pedroollero1998@gmail.com>
2025-03-13 12:04:18 +0100

    Calculate and display the cost per person for a dinner invitation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
-import "./style.css";
+let amigos: number = 6;
+let precioCena: number = 120;
+let invitoYo: number = 18;
 
-console.log("Hello Typescript!");
+const cuantoPago: number = (precioCena - invitoYo)/(amigos + 1);
+
+//Aquí paso a dos decimales
+let resultado: string = cuantoPago.toFixed(2)
+
+console.log(resultado)
